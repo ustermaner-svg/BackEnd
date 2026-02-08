51a441805cbe25d14f9cb9b96a46b66dedfa734b
ustermaner-svg <ustermaner@gmail.com>
2026-02-08 19:00:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express(); //this is like creating an object of the class express in
 const cors = require("cors"); // this is like #include in c++
 app.use(cors()); // this is like #include in c++
 app.use(express.json()); // this tells the code to be ready to handle json
-
+const VERSION = "1.8";
 // Root endpoint
 app.get("/", (req, res) => {
     res.send("Hello from Node.js backend!"); // confirmation that the server is running and can respond to requests
 });
 app.get("/version", (req,res)=>{
-    res.send("Version TESTING BACKEND");
+    res.send("Version "+VERSION+" BACKEND");
 });
 const ACCESS_KEY = 'VT9QiRxg_zkaEx5z2PAO3tAJ-2XSwLgdGapDJ9orNo8';
 // Age + Gender endpoint
@@ -19,10 +19,10 @@ app.post("/getAgeGender", async (req, res) => {
     const { name, query } = req.body;
     try {
         console.log("Fetching age...");
-        const ageResponse = {data: 1}; //await axios.get(`https://api.agify.io?name=${name}`);
+        const ageResponse = await axios.get(`https://api.agify.io?name=${name}`);
         console.log("Age OK:", ageResponse.data);
         console.log("Fetching gender...");
-        const genderResponse = {data: 4};// await axios.get(`https://api.genderize.io?name=${name}`);
+        const genderResponse = await axios.get(`https://api.genderize.io?name=${name}`);
         console.log("Gender OK:", genderResponse.data);
         console.log("Fetching Unsplash...");
         const imageResponse = await axios.get(
@@ -51,6 +51,7 @@ app.post("/getAgeGender", async (req, res) => {
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
+
 
 
 
